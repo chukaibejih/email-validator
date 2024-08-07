eb59e3ac44efe55995584821b6b9db56c03201ef
chukaibejih <chukaibejih@gmail.com>
2024-08-07 14:38:04 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text(encoding='utf-8')
 
 setup(
     name="email-safeguard",
-    version="0.1.0",
+    version="0.1.1",
     author="Chukwuka Ibejih",
     author_email="chukaibejih@gmail.com",
     description="A Python library for validating and suggesting corrections for email addresses.",
